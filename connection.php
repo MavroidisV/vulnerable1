a3d05ca9f6d78bce6e009c0937103aef66d3fe02
mavro <mavroidis@hotmail.gr>
2016-04-13 15:20:15 +0100

    test

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'us-cdbr-azure-west-c.cloudapp.net');
 define('DB_USERNAME', 'bf7e72f7bbbf38');
 define('DB_PASSWORD', '783262c1');
-define('DB_DATABASE', 'acsm_2a03fd15ec74d01;Data Source=us-cdbr-azure-west-c.cloudapp.net;User Id=bf7e72f7bbbf38;Password=783262c1');
+define('DB_DATABASE', 'acsm_2a03fd15ec74d01');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 ?>
