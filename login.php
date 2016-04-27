5fe7f29b125057b0483673c115f56965350216ad
mavro <mavroidis@hotmail.gr>
2016-04-27 15:47:26 +0100

    test

--- login.php
+++ login.php
@@ -26,8 +26,8 @@ error_reporting(E_ALL);
 			if (!($data=$db->prepare("SELECT userID FROM users WHERE username=? and password=?;")))
 			{echo "fail";}
 	
-			$data->bind_param(1,$username1 );
-			$data->bind_param(2,$password );
+			$data->bind_param('s',$username1 );
+			$data->bind_param('s',$password );
 
 			
 			
