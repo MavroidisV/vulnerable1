19d2e64980c0cf294250ae0c31482bc294d1a5ab
mavro <mavroidis@hotmail.gr>
2016-04-27 15:29:44 +0100

    test

--- login.php
+++ login.php
@@ -45,8 +45,8 @@ error_reporting(E_ALL);
 			
 			//If username and password exist in our database then create a session.
 			//Otherwise echo error.
-			
-			if(mysqli_num_rows($data) == 1)
+			if ($data->rowCount()==1)
+			//if(mysqli_num_rows($data) == 1)
 			{
 				$_SESSION['username'] = $username; // Initializing Session
 				header("location: photos.php"); // Redirecting To Other Page
