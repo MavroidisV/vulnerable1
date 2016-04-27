f21cb525362a5ac09b86897f328f6d1e80663600
mavro <mavroidis@hotmail.gr>
2016-04-27 09:48:09 +0100

    test

--- login.php
+++ login.php
@@ -23,10 +23,10 @@ error_reporting(E_ALL);
 
 			
 			//Check username and password from database
-			$data=$db->prepare=('SELECT userID FROM users WHERE username= (:username) and password= (:password);');
+			$data=$db->prepare=('SELECT userID FROM users WHERE username=? and password=?;');
 			
-			$data->bind_param(":username",$password );
-			$data->bind_param(":password",$password);
+			$data->bind_param('s',$username );
+			$data->bind_param('s',$password);
 			$data -> execute();
 			//$row=$data->fetch();
 			$result=mysqli_query($db,$data);
