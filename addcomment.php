da8f8e56f4a0a5bb7204ef8c327fcb1d076936d0
mavro <mavroidis@hotmail.gr>
2016-04-30 17:04:42 +0100

    test

--- addcomment.php
+++ addcomment.php
@@ -3,8 +3,7 @@ session_start();
 include("connection.php"); //Establishing connection with our database
 
 $msg = ""; //Variable for storing our errors.
-if(isset($_POST["submit"]))
-{
+if(isset($_POST["submit"])) {
 
     $desc = $_POST["desc"];
     $photoID = $_POST["photoID"];
@@ -13,52 +12,51 @@ if(isset($_POST["submit"]))
     // To protect from MySQL injection and XSS
     $desc = stripslashes($desc);
     $photoID = stripslashes($photoID);
-    
+
     $desc = mysqli_real_escape_string($db, $desc);
     $photoID = mysqli_real_escape_string($db, $photoID);
     $desc = htmlspecialchars($desc);
     $photoID = htmlspecialchars($photoID);
 
-    if (!($data=$db->prepare("SELECT userID FROM users WHERE username=?;")))
-    {echo "fail";}
+    if (!($data = $db->prepare("SELECT userID FROM users WHERE username=?;"))) {
+        echo "fail";
+    }
 
-    if(!$data->bind_param('s',$name)) {
+    if (!$data->bind_param('s', $name)) {
         echo "binding parameters failed: (" . $data->errno . ")" . $data->error;
     }
 
 
-    if (!$data -> execute()){
+    if (!$data->execute()) {
         echo "Execute failed: (" . $data->errno . ") " . $data->error;
     }
 
     $data->store_result(); //store_result() "binds" the last given answer to the statement-object for... reasons. Now we can use it
 
     //$sql="SELECT userID FROM users WHERE username='$name'";
-   // $result=mysqli_query($db,$sql);
-   // $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-  //  if(mysqli_num_rows($result) == 1) {
-    $row=$data->fetch();
-    if ($data->num_rows == "1")
+    // $result=mysqli_query($db,$sql);
+    // $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+    //  if(mysqli_num_rows($result) == 1) {
+    $row = $data->fetch();
+    //if ($data->num_rows == "1")
     {
         /* Bind the result to variables */
-       // $data->bind_result($id);
+        // $data->bind_result($id);
 
         //$row=$data->fetch();
         //echo $name." ".$email." ".$password;
-       // $id = $row['userID'];
+        // $id = $row['userID'];
 
 
-        $data=$db->prepare( "INSERT INTO comments (description, userID,photoID) VALUES (?,?,?)");
-        $data->bind_param("sss", $desc, $id,$photoID);
-       // $query = mysqli_query($db, $addsql) or die(mysqli_error($db));
+        $data = $db->prepare("INSERT INTO comments (description, userID,photoID) VALUES (?,?,?)");
+        $data->bind_param("sss", $desc, $id, $photoID);
+        // $query = mysqli_query($db, $addsql) or die(mysqli_error($db));
         $data->execute();
         if ($data) {
-            $msg = "Thank You! comment added. click <a href='photo.php?id=".$photoID."'>here</a> to go back";
+            $msg = "Thank You! comment added. click <a href='photo.php?id=" . $photoID . "'>here</a> to go back";
+        } else {
+            $msg = "You need to login first";
         }
     }
-    else{
-        $msg = "You need to login first";
-    }
 }
-
 ?>
