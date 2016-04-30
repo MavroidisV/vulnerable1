f14c8697def054bc58ba9f628e9ca5d14662c737
mavro <mavroidis@hotmail.gr>
2016-04-30 16:33:00 +0100

    test

--- addcomment.php
+++ addcomment.php
@@ -10,10 +10,40 @@ if(isset($_POST["submit"]))
     $photoID = $_POST["photoID"];
     $name = $_SESSION["username"];
 
-    $sql="SELECT userID FROM users WHERE username='$name'";
-    $result=mysqli_query($db,$sql);
-    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-    if(mysqli_num_rows($result) == 1) {
+    // To protect from MySQL injection and XSS
+    $desc = stripslashes($desc);
+    $photoID = stripslashes($photoID);
+    
+    $desc = mysqli_real_escape_string($db, $desc);
+    $photoID = mysqli_real_escape_string($db, $photoID);
+    $desc = htmlspecialchars($desc);
+    $photoID = htmlspecialchars($photoID);
+
+    if (!($data=$db->prepare("SELECT userID FROM users WHERE username=?;")))
+    {echo "fail";}
+
+    if(!$data->bind_param('s',$name)) {
+        echo "binding parameters failed: (" . $data->errno . ")" . $data->error;
+    }
+
+
+    if (!$data -> execute()){
+        echo "Execute failed: (" . $data->errno . ") " . $data->error;
+    }
+
+    $data->store_result(); //store_result() "binds" the last given answer to the statement-object for... reasons. Now we can use it
+
+    //$sql="SELECT userID FROM users WHERE username='$name'";
+   // $result=mysqli_query($db,$sql);
+   // $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+  //  if(mysqli_num_rows($result) == 1) {
+
+    if ($data->num_rows >= "1")
+    {
+        /* Bind the result to variables */
+        $data->bind_result($id);
+
+        $row=$data->fetch();
         //echo $name." ".$email." ".$password;
         $id = $row['userID'];
         $addsql = "INSERT INTO comments (description, postDate,photoID,userID) VALUES ('$desc',now(),'$photoID','$id')";
