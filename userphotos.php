f7c63a0227d630d756d025005b152ddd117fa54c
mavro <mavroidis@hotmail.gr>
2016-04-30 14:07:59 +0100

    test

--- userphotos.php
+++ userphotos.php
@@ -4,8 +4,7 @@ if(isset($_SESSION['username']))
 {
     $name = $_SESSION["username"];
 
-
-
+    
 
     //Check username from db
     if (!($data=$db->prepare("SELECT userID FROM users WHERE username=?;")))
@@ -24,15 +23,31 @@ if(isset($_SESSION['username']))
 
 
 
-    if ($data->num_rows >= "1")
+    if ($data->num_rows == "1")
+    
+    
+    
 
-        
+   /* $sql="SELECT userID FROM users WHERE username='$name'";
+    $result=mysqli_query($db,$sql);
+    $row=mysqli_fetch_assoc($result);
+    if(mysqli_num_rows($result) == 1)
+   
+   */
+   
     {
+
+        /* Bind the result to variables */
+        $data->bind_result($id);
+
+        $row=$data->fetch();
+
         $searchID = $row['userID'];
         $searchSql="SELECT title, photoID,url FROM photos WHERE userID='$searchID'";
         $searchresult=mysqli_query($db,$searchSql);
 
-        if(mysqli_num_rows($searchresult)>0){
+        if(mysqli_num_rows($searchresult)>0)
+   {
             while($searchRow = mysqli_fetch_assoc($searchresult)){
                 $line = "<p><img src='".$searchRow['url']."' style='width:100px;height:100px;'><a href='photo.php?id=".$searchRow['photoID']."'>".$searchRow['title']."</a></p>";
                 $resultText = $resultText.$line;
