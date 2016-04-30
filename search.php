e260b87a2bcc3b4e2198274bb9fad60ad0cbf77d
mavro <mavroidis@hotmail.gr>
2016-04-30 11:18:24 +0100

    test

--- search.php
+++ search.php
@@ -19,9 +19,9 @@ if(isset($_POST["submit"]))
     }
 
     $data->store_result(); //store_result() "binds" the last given answer to the statement-object for... reasons. Now we can use it
-   // $row=$data->fetch();
-   // echo $row;
-    echo $data;
+    $row=$data->fetch();
+    echo $row;
+    
 
 ////////
 
