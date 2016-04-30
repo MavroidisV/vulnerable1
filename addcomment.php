550f11f75d8ff537f9096fc79a64309c2022339d
mavro <mavroidis@hotmail.gr>
2016-04-30 17:35:07 +0100

    test

--- addcomment.php
+++ addcomment.php
@@ -1,9 +1,6 @@
 <?php
 
-//display error
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 
 session_start();
 include("connection.php"); //Establishing connection with our database
@@ -45,19 +42,24 @@ if(isset($_POST["submit"])) {
     //  if(mysqli_num_rows($result) == 1) {
     $row = $data->fetch();
     //if ($data->num_rows == "1")
+    {
+        /* Bind the result to variables */
+        // $data->bind_result($id);
 
-    /* Bind the result to variables */
-    // $data->bind_result($id);
+        //$row=$data->fetch();
+        //echo $name." ".$email." ".$password;
+        // $id = $row['userID'];
 
-    //$row=$data->fetch();
-    //echo $name." ".$email." ".$password;
-    // $id = $row['userID'];
-echo "ok upto now";
-
-    $data = $db->prepare("INSERT INTO comments (description,photoID) VALUES (?,?)") or die(mysqli_error($db));
-    $data->bind_param("ss", $desc, $photoID);
-    $data->execute();
-    // $query = mysqli_query($db, $addsql) or die(mysqli_error($db));
-    
+        if ($data->num_rows >= "1"){
+        $data = $db->prepare("INSERT INTO comments (description,photoID) VALUES (?,?)")or die(mysqli_error($db));
+        $data->bind_param("ss",$desc,$photoID);
+        $data->execute();
+        // $query = mysqli_query($db, $addsql) or die(mysqli_error($db));
+            
+            $msg = "Thank You! comment added. click <a href='photo.php?id=" . $photoID . "'>here</a> to go back";
+        } else {
+            $msg = "You need to login first";
+        }
+    }
 }
 ?>
