a8648fd68ca07534a849ec63b2bb08c872979d98
mavro <mavroidis@hotmail.gr>
2016-04-30 13:24:19 +0100

    test

--- search.php
+++ search.php
@@ -1,35 +1,42 @@
 <?php
 $resultText = "";
-if(isset($_POST["submit"])) {
+if(isset($_POST["submit"]))
+{
     $name = $_POST["username"];
 
 
     //Check username from db
-    if (!($data = $db->prepare("SELECT userID FROM users WHERE username=?;"))) {
-        echo "fail";
-    }
+    if (!($data=$db->prepare("SELECT userID FROM users WHERE username=?;")))
+    {echo "fail";}
 
-    if (!$data->bind_param('s', $name)) {
+    if(!$data->bind_param('s',$name)) {
         echo "binding parameters failed: (" . $data->errno . ")" . $data->error;
     }
 
 
-    if (!$data->execute()) {
+    if (!$data -> execute()){
         echo "Execute failed: (" . $data->errno . ") " . $data->error;
     }
 
     $data->store_result(); //store_result() "binds" the last given answer to the statement-object for... reasons. Now we can use it
 
 
-    if ($data->num_rows >= "1") {
-        /* Bind the result to variables */
-        $data->bind_result($id);
 
-        $row = $data->fetch();
+    if ($data->num_rows >= "1")
+   {
+       /* Bind the result to variables */
+       $data->bind_result($id);
+
+       $row=$data->fetch();
 
 
-        $searchSql="SELECT title, photoID FROM photos WHERE userID='$searchID'";
-        $searchresult=mysqli_query($db,$searchSql);
+
+       
+
+
+
+       $searchSql="SELECT title, photoID FROM photos WHERE userID='$id'";
+       $searchresult=mysqli_query($db,$searchSql);
 
         if(mysqli_num_rows($searchresult)>0){
             while($searchRow = mysqli_fetch_assoc($searchresult)){
