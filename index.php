736edf04f3974b616774aa301c943aae7bc07bfd
mavro <mavroidis@hotmail.gr>
2016-05-01 16:34:36 +0100

    test

--- index.php
+++ index.php
@@ -28,8 +28,8 @@
             <input type="password" name="password" placeholder="password" />  <br><br><br>
             <label>Captcha:</label><br>
             <p><img src="/captcha.php" width="120" height="30" border="1" alt="CAPTCHA"></p>
-            <p><input type="text" size="6" maxlength="5" name="captcha" value=""></p><br>
-                <small>copy the digits from the image into this box</small><br>
+            <p><input type="text" size="6" maxlength="5" name="captcha" value=""></p>
+                <small>copy the digits from the image into this box</small><br><br>
             <input type="submit" name="submit" value="Login" />
         </form>
         <div class="error"><?php echo $error;?></div>
