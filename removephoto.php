3c2d949aa2560ae00fff337922da19c314a3c063
mavro <mavroidis@hotmail.gr>
2016-05-01 13:08:23 +0100

    test

--- removephoto.php
+++ removephoto.php
@@ -6,9 +6,27 @@ include("connection.php"); //Establishing connection with our database
 if(isset($_GET['id']))
 {
     $photoID = $_GET['id'];
-    $remsql = "DELETE FROM photos WHERE photoID='$photoID'";
-    $query = mysqli_query($db, $remsql) or die(mysqli_error($db));
-    if ($query) {
+
+
+
+    //Check username from db
+    if (!($data=$db->prepare("DELETE FROM photos FROM photos WHERE photoID=?;")))
+    {echo "fail";}
+
+    if(!$data->bind_param('s',$photoID)) {
+        echo "binding parameters failed: (" . $data->errno . ")" . $data->error;
+    }
+
+    if (!$data -> execute()){
+        echo "Execute failed: (" . $data->errno . ") " . $data->error;
+    }
+
+    //$data->store_result(); //store_result() "binds" the last given answer to the statement-object for... reasons. Now we can use it
+
+    /* Bind results to variables */
+    $data->bind_result($commentID, $description, $postDate, $userID, $photoID);
+    
+    if ($data) {
         header("Location: photos.php");
     }
     else {
