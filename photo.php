882f98d1bc9a16b1f21c3aa07c45719001d6351a
mavro <mavroidis@hotmail.gr>
2016-05-01 13:28:38 +0100

    test

--- photo.php
+++ photo.php
@@ -33,29 +33,30 @@ else {$_SESSION['timeout']=time();}
 <h4>Welcome <?php echo $login_user;?> <a href="photos.php" style="font-size:18px">Photos</a>||<a href="searchphotos.php" style="font-size:18px">Search</a>||<a href="logout.php" style="font-size:18px">Logout</a></h4>
 <div id="photo">
     <?php
-        if(isset($_GET['id'])){
+        if(isset($_GET['id'])) {
             $photoID = $_GET['id'];
 
             //Check username from db
-            if (!($data=$db->prepare("SELECT photoID, title, description, postDate, url, userID FROM photos WHERE photoID=?;")))
-            {echo "fail";}
+            if (!($data = $db->prepare("SELECT photoID, title, description, postDate, url, userID FROM photos WHERE photoID=?;"))) {
+                echo "fail";
+            }
 
-            if(!$data->bind_param('s',$photoID)) {
+            if (!$data->bind_param('s', $photoID)) {
                 echo "binding parameters failed: (" . $data->errno . ")" . $data->error;
             }
 
-            if (!$data -> execute()){
+            if (!$data->execute()) {
                 echo "Execute failed: (" . $data->errno . ") " . $data->error;
             }
 
             //$data->store_result(); //store_result() "binds" the last given answer to the statement-object for... reasons. Now we can use it
-            
+
             /* Bind results to variables */
             $data->bind_result($photoID, $title, $description, $postDate, $url, $userID);
             //if ($data->num_rows = "1")
-           // {
-                /* Bind the result to variables */
-                //$data->bind_result($id,$title,$description,$postDate,$url,$userID);
+            // {
+            /* Bind the result to variables */
+            //$data->bind_result($id,$title,$description,$postDate,$url,$userID);
 
             /* fetch values */
             while ($data->fetch()) {
@@ -66,45 +67,46 @@ else {$_SESSION['timeout']=time();}
                 echo " <p>" . $description . "</p>";
 
             }
-            
 
-    //Check username from db
-    if (!($data=$db->prepare("SELECT commentID,description,postDate,userID,photoID FROM comments WHERE photoID=?;")))
-    {echo "fail";}
 
-    if(!$data->bind_param('s',$photoID)) {
-        echo "binding parameters failed: (" . $data->errno . ")" . $data->error;
-    }
+            //Check username from db
+            if (!($data = $db->prepare("SELECT commentID,description,postDate,userID,photoID FROM comments WHERE photoID=?;"))) {
+                echo "fail";
+            }
 
-    if (!$data -> execute()){
-        echo "Execute failed: (" . $data->errno . ") " . $data->error;
-    }
+            if (!$data->bind_param('s', $photoID)) {
+                echo "binding parameters failed: (" . $data->errno . ")" . $data->error;
+            }
 
-    //$data->store_result(); //store_result() "binds" the last given answer to the statement-object for... reasons. Now we can use it
+            if (!$data->execute()) {
+                echo "Execute failed: (" . $data->errno . ") " . $data->error;
+            }
 
-    /* Bind results to variables */
-    $data->bind_result($commentID, $description, $postDate, $userID, $photoID);
- 
+            //$data->store_result(); //store_result() "binds" the last given answer to the statement-object for... reasons. Now we can use it
+
+            /* Bind results to variables */
+            $data->bind_result($commentID, $description, $postDate, $userID, $photoID);
+
             echo "<h2> Comments </h2>";
-    while ($data->fetch()) {
+            while ($data->fetch()) {
 
-        
-                        echo "<div class = 'comments'>";
-                        echo "<h3>".$postDate."</h3>";
-                        echo "<p>".$commentID."</p>";
-                        echo "<p>".$description."</p>";
-        
-                        echo "</div>";
-                    }
 
-                }
-    echo "<a href='addcommentform.php?id=".$photoID."'> Add Comment</a><br>";
+                echo "<div class = 'comments'>";
+                echo "<h3>" . $postDate . "</h3>";
+                echo "<p>" . $commentID . "</p>";
+                echo "<p>" . $description . "</p>";
 
-    if($adminuser){
-        echo "<div class='error'><a href='removephoto.php?id=".$photoID."'> Delete Photo</a></div>";
-                }
+                echo "</div>";
+            }
 
-         
+
+            echo "<a href='addcommentform.php?id=" . $photoID . "'> Add Comment</a><br>";
+
+            if ($adminuser) {
+                echo "<div class='error'><a href='removephoto.php?id=" . $photoID . "'> Delete Photo</a></div>";
+            }
+
+        }
       // }
                  
 
