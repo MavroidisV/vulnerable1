ba7b6043f3c6116ff6796a5d60731942709bbd13
mavro <mavroidis@hotmail.gr>
2016-05-01 18:56:58 +0100

    test

--- addphoto.php
+++ addphoto.php
@@ -72,6 +72,11 @@ if(isset($_POST["submit"])) {
         //check file size
             //if ($_FILES["fileToUpload"]["size"]>500000){echo "sorry your file is too large";
                  //$uploadOk=0;}
+    $check=getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check!==false){echo "file is an image-" .$check["mime"].".";
+    $uploadOk=1;}
+    else {echo "file isnt an image.";
+    $uploadOk=0;}
 
         if ($uploadOk == 1) {
             if (!($data = $db->prepare("INSERT INTO photos (title,description, postDate,url,userID) VALUES (?,?,?,?,?)"))) {
