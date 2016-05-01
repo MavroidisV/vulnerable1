bd03de998afc82d568f08c9007c11d3511518537
mavro <mavroidis@hotmail.gr>
2016-05-01 11:38:04 +0100

    test

--- check.php
+++ check.php
@@ -2,6 +2,17 @@
 include('connection.php');
 session_start();
 $user_check=$_SESSION['username'];
+
+//////
+$_SESSION['logged_in'] = true; //set you've logged in
+$_SESSION['last_activity'] = time(); //your last activity was now, having logged in.
+$_SESSION['expire_time'] = 1*60;
+
+
+////
+
+
+
 $_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
 
 $ses_sql = mysqli_query($db,"SELECT username, admin FROM users WHERE username='$user_check' ");
